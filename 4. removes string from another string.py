8d2b720892aa3f8b49c2e033cb168352d9c092a8
Santosh Gupta <gsantoshkumar862@gmail.com>
2025-01-28 12:27:06 +0530

    Delete 4. removes string from another string.py

--- 4. removes string from another string.py
+++
@@ -1,8 +0,0 @@
-#4. removes one string from another string
-
-def remove_substring(string1, string2):
-    return string1.replace(string2, '')
-string1 = input("Enter a string: ")
-string2 = input("Enter string to remove: ")
-finalstring = remove_substring(string1, string2)
-print(finalstring)
