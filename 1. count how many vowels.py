da78ab0a724ee0b7b9a92de93a6119aec12331ab
Santosh Gupta <gsantoshkumar862@gmail.com>
2025-01-28 12:26:28 +0530

    Delete 1. count how many vowels.py

--- 1. count how many vowels.py
+++
@@ -1,8 +0,0 @@
-# count vowels
-sentence = input("ente a string:")
-vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-count = 0
-for char in sentence:
-    if char in vowels:
-        count = count + 1
-print("number of vowels in a given string is:", count)
